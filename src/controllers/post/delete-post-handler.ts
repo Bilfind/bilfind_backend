0ae990612b4954b8e195c47c9001e52343db0f3e
Serhat Merak <42006836+merakserhat@users.noreply.github.com>
2023-12-16 22:25:51 +0300

    feat: remove get comment

--- src/controllers/post/delete-post-handler.ts
+++ src/controllers/post/delete-post-handler.ts
@@ -14,28 +14,28 @@ const deletePostHandler = async (req: Request, res: Response) => {
     const { postId } = req.query;
 
     if (!postId || typeof postId !== "string") {
-        return ApiHelper.getErrorResponseForCrash(res, "Post Id must be given");
+      return ApiHelper.getErrorResponseForCrash(res, "Post Id must be given");
     }
 
-    const post = await PostClient.getCommentById(postId);
+    const post = await PostClient.getPostById(postId);
 
     if (!post) {
-        return ApiHelper.getErrorResponseForCrash(res, "Post could not be found");
+      return ApiHelper.getErrorResponseForCrash(res, "Post could not be found");
     }
 
     // @ts-ignore
     const locals = req.locals;
     const user: User = locals.user;
     const userId = user._id!.toString();
-    
+
     if (userId !== post.userId) {
-        return ApiHelper.getErrorResponseForCrash(res, "Post does not belong to this user");
+      return ApiHelper.getErrorResponseForCrash(res, "Post does not belong to this user");
     }
-    
+
     const isDeleted = await PostClient.updatePostStatus(post._id!.toString(), PostStatus.DELETED);
 
     if (!isDeleted) {
-        return ApiHelper.getErrorResponseForCrash(res, "Post could not be deleted");
+      return ApiHelper.getErrorResponseForCrash(res, "Post could not be deleted");
     }
 
     return ApiHelper.getSuccessfulResponse(res);
